fcda4a594179dc62c9e128eed48bbd89bf34aff1
Naohiro OHTA <naohta@gmail.com>
2015-06-05 01:48:41 +0900

    Camera button

--- hello.swift.Camera/Glass.swift
+++ hello.swift.Camera/Glass.swift
@@ -12,13 +12,16 @@ class Glass: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
+
+    @IBAction func cameraButtonDidTouchUp(sender: UIButton) {
+        
+
+    }
+
+
 }
