ad608b0336b03cee292ecb8834ab6234b11acb85
Naohiro OHTA <naohta@gmail.com>
2015-06-05 01:32:14 +0900

    Glass

---
+++ hello.swift.Camera/Glass.swift
@@ -0,0 +1,24 @@
+//
+//  Glass.swift
+//  hello.swift.Camera
+//
+//  Created by Naohiro OHTA on Jun5,2015.
+//  Copyright (c) 2015 Naohiro OHTA. All rights reserved.
+//
+
+import UIKit
+
+class Glass: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
