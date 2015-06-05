1468dc4c21ac5a4c966c072a41ef006fe9c6eeba
Naohiro OHTA <naohta@gmail.com>
2015-06-05 12:58:09 +0900

    delete a delegate class

--- hello.swift.Camera/FlatGlass.swift
+++ hello.swift.Camera/FlatGlass.swift
@@ -8,9 +8,49 @@
 
 import UIKit
 
-class FlatGlass: UIViewController {
+
+class FlatGlass: UIViewController,
+                 UIImagePickerControllerDelegate,
+                 UINavigationControllerDelegate {
+
+
+	// MARK: - Outlets
+    // ---------------
 
     @IBOutlet weak var myImageView: UIImageView!
+
+
+	// MARK: - App tells picker (or his delegate) 
+    //         when user finished picking a media
+	// ------------------------------------------
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+
+        if let i = info[UIImagePickerControllerOriginalImage] as? UIImage {
+			myImageView.image = i
+        }
+
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+    // MARK: - Show Camera
+	// -------------------
+
+    @IBAction func cameraButtonDidTouchUp(sender: UIButton) {
+        var picker = UIImagePickerController()
+        let camera = UIImagePickerControllerSourceType.Camera
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            picker.sourceType = camera
+            picker.delegate = self
+            presentViewController(picker, animated: true, completion: nil)
+        }
+
+    }
+
+
+	// MARK: - Common VC
+    // -----------------
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,23 +59,5 @@ class FlatGlass: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
-    // MARK: -
-
-    let myDelegate = Delegate_of_ImagePickerController()
-
-    @IBAction func cameraButtonDidTouchUp(sender: UIButton) {
-        var picker = UIImagePickerController()
-        let camera = UIImagePickerControllerSourceType.Camera
-        if UIImagePickerController.isSourceTypeAvailable(camera) {
-            picker.sourceType = camera
-            myDelegate.imageView = myImageView
-            picker.delegate = myDelegate
-            presentViewController(picker, animated: true, completion: nil)
-        }
-
-    }
-
 
 }
